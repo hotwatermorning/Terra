fcfe5a4af1ee311fc37f42da423c76dbe61f1d5e
hotwatermorning <hotwatermorning@gmail.com>
2019-02-09 03:17:37 +0900

    [device] add equality operator overloads to MidiDeviceInfo

--- Terra/device/MidiDevice.hpp
+++ Terra/device/MidiDevice.hpp
@@ -8,6 +8,17 @@ struct MidiDeviceInfo
 {
     DeviceIOType io_type_;
     String name_id_;
+    
+    bool operator==(MidiDeviceInfo const &rhs) const
+    {
+        return  (this->name_id_ == rhs.name_id_)
+        &&      (this->io_type_ == rhs.io_type_);
+    }
+    
+    bool operator!=(MidiDeviceInfo const &rhs) const
+    {
+        return !(*this == rhs);
+    }
 };
 
 class MidiDevice

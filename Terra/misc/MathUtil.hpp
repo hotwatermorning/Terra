9fd05c26217a234d58ce4ad8dd4435c22be61e92
hotwatermorning <hotwatermorning@gmail.com>
2019-02-09 03:33:26 +0900

    [misc] add Clamp() to MathUtil.hpp

--- Terra/misc/MathUtil.hpp
+++ Terra/misc/MathUtil.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <algorithm>
 
 NS_HWM_BEGIN
 
@@ -9,6 +10,18 @@ inline
 Dest Round(Src src)
 {
     return static_cast<Dest>(std::round(src));
+}
+
+template <class T>
+constexpr const T& Clamp(const T& v, const T& low, const T& high)
+{
+    return std::min<T>(std::max<T>(v, low), high);
+}
+
+template <class T, class Compare>
+constexpr const T& Clamp(const T& v, const T& low, const T& high, Compare comp)
+{
+    return std::min<T>(std::max<T>(v, low, comp), high, comp);
 }
 
 NS_HWM_END

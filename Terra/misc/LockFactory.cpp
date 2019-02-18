b96f645517f32a0ab1d46e5769515668a54960d6
hotwatermorning <hotwatermorning@gmail.com>
2019-02-18 21:27:12 +0900

    [misc] add try lock functions to LockFactory

--- Terra/misc/LockFactory.cpp
+++ Terra/misc/LockFactory.cpp
@@ -9,4 +9,8 @@ std::unique_lock<std::mutex> LockFactory::make_lock() const {
     return std::unique_lock<std::mutex>(mtx_);
 }
 
+std::unique_lock<std::mutex> LockFactory::make_lock(std::try_to_lock_t try_to_lock) const {
+    return std::unique_lock<std::mutex>(mtx_, try_to_lock);
+}
+
 NS_HWM_END
--- Terra/misc/LockFactory.hpp
+++ Terra/misc/LockFactory.hpp
@@ -15,6 +15,8 @@ private:
     
 public:
     [[nodiscard]] std::unique_lock<std::mutex> make_lock() const;
+    [[nodiscard]] std::unique_lock<std::mutex> make_lock(std::try_to_lock_t try_to_lock) const;
+    [[nodiscard]] std::unique_lock<std::mutex> try_make_lock() const { return make_lock(std::try_to_lock); }
 };
 
 NS_HWM_END

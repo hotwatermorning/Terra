c6fb623118d0e31a0ab4719dc9fd82a56a96b7b9
hotwatermorning <hotwatermorning@gmail.com>
2019-07-14 19:19:33 +0900

    [test/LoggerTest] add test cases

--- Terra/test/LoggerTest.cpp
+++ Terra/test/LoggerTest.cpp
@@ -1,6 +1,12 @@
 #include "catch2/catch.hpp"
 
 #include <fstream>
+#include <thread>
+#include <atomic>
+#include <condition_variable>
+#include <string_view>
+
+#include <wx/ffile.h>
 
 #include "../log/Logger.hpp"
 #include "../log/LoggingStrategy.hpp"
@@ -173,4 +179,186 @@ TEST_CASE("FileLoggingStrategy test", "[log]")
 #else
     REQUIRE(s == "hello\nworld\n\nc++\n");
 #endif
+}
+
+TEST_CASE("global logger access test", "[log]")
+{
+    CHECK(GetGlobalLogger() == nullptr);
+    
+    auto tmp = std::make_unique<Logger>();
+    auto new_logger = tmp.get();
+    auto prev = ReplaceGlobalLogger(std::move(tmp));
+    
+    CHECK(prev == nullptr);
+    CHECK(GetGlobalLogger() == new_logger);
+    
+    int const kMaxShare = 50;
+    std::atomic<bool> start_trying_to_replace {false};
+    std::atomic<bool> still_blocked {true};
+    
+    std::vector<LoggerRef> ls;
+    for(int i = 0; i < kMaxShare; ++i) {
+        ls.push_back(GetGlobalLogger());
+    }
+    
+    auto th = std::thread([&] {
+        start_trying_to_replace.store(true);
+        
+        // ReplaceGlobalLogger() will be blocked until all LoggerRefs are destructed.
+        ReplaceGlobalLogger(nullptr);
+        still_blocked.store(false);
+    });
+    
+    for( ; ; ) {
+        if(start_trying_to_replace.load()) { break; }
+        std::this_thread::yield();
+    }
+    
+    for(int i = 0; i < kMaxShare; ++i) {
+        REQUIRE(still_blocked.load() == true);
+        ls.pop_back();
+        std::this_thread::yield();
+    }
+    
+    for(Int32 i = 0; i < 100; ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        if(still_blocked.load() == false) {
+            break;
+        }
+    }
+    
+    CHECK(still_blocked.load() == false);
+    
+    th.join();
+}
+
+TEST_CASE("multithread logging test", "[log]")
+{
+    auto st = std::make_shared<TestLoggingStrategy>();
+    Logger lg;
+    
+    lg.SetLoggingLevels({L"Info"});
+    lg.SetStrategy(st);
+    lg.StartLogging(true);
+    
+    int const kNumThreads = 50;
+    int const kMsgPerThread = 1000;
+    bool is_ready_to_output = false;
+    LockFactory lf;
+    std::condition_variable cv;
+    
+    std::vector<std::thread> ths;
+    for(int t = 0; t < kNumThreads; ++t) {
+        ths.push_back(std::thread([&, t] {
+            {
+                auto lock = lf.make_lock();
+                cv.wait(lock, [&] { return is_ready_to_output; });
+            }
+            
+            for(int i = 0; i < kMsgPerThread; ++i) {
+                lg.OutputLog(L"Info", [&, t, i]{ return L"[{:02d}]({:03d}): hello"_format(t, i); });
+            }
+        }));
+    }
+    
+    {
+        auto lock = lf.make_lock();
+        is_ready_to_output = true;
+    }
+
+    // let all threads start logging.
+    cv.notify_all();
+    
+    // all threads should successfully finished.
+    for(int t = 0; t < kNumThreads; ++t) {
+        ths[t].join();
+    }
+    
+    CHECK(st->history.size() == kNumThreads * kMsgPerThread);
+    
+    std::vector<std::wstring_view> tmp;
+    tmp.reserve(kNumThreads * kMsgPerThread);
+    for(auto const &s: st->history) {
+        tmp.push_back(std::wstring_view(s).substr(s.size() - 16, String::npos));
+    }
+    std::sort(tmp.begin(), tmp.end());
+    
+    for(int t = 0; t < kNumThreads; ++t) {
+        for(int i = 0; i < kMsgPerThread; ++i) {
+            if(tmp[t * kMsgPerThread + i] != L"[{:02d}]({:03d}): hello"_format(t, i)) {
+                REQUIRE(tmp[t * kMsgPerThread + i] == L"[{:02d}]({:03d}): hello"_format(t, i));
+            }
+        }
+    }
+}
+
+TEST_CASE("logfile rotation test", "[log]")
+{
+    auto get_file_size = [](wxFileName path) {
+        wxFFile tmp(path.GetFullPath(), "rb");
+        assert(tmp.IsOpened());
+        return tmp.Length();
+    };
+    
+    SECTION("rotate function test") {
+        TestApp app;
+        auto scoped_dir = ScopedTemporaryDirectoryProvider(L"logging-test");
+        
+        auto const test_file_name = L"logging-test.log";
+        auto const test_file_path = wxFileName(scoped_dir.GetPath(), test_file_name);
+
+#if defined(_MSC_VER)
+        std::ofstream ofs(test_file_path.GetFullPath().ToStdWstring(), std::ios::binary|std::ios::out|std::ios_base::trunc);
+#else
+        std::ofstream ofs(test_file_path.GetFullPath().ToStdString(), std::ios::binary|std::ios::out|std::ios_base::trunc);
+#endif
+        
+        REQUIRE(ofs.is_open());
+        
+        std::string test_str = "hello, world";
+        ofs.write(test_str.data(), test_str.size());
+        ofs.close();
+        REQUIRE(get_file_size(test_file_path) == test_str.size());
+        
+        auto err = FileLoggingStrategy::Rotate(test_file_path.GetFullPath().ToStdWstring(), 4);
+        CHECK(err.has_error() == false);
+        REQUIRE(get_file_size(test_file_path) == 4);
+        
+        std::string tmp;
+        tmp.resize(4);
+        
+#if defined(_MSC_VER)
+        std::ifstream ifs(test_file_path.GetFullPath().ToStdWstring(), std::ios::binary|std::ios::in);
+#else
+        std::ifstream ifs(test_file_path.GetFullPath().ToStdString(), std::ios::binary|std::ios::in);
+#endif
+        
+        ifs.read(tmp.data(), tmp.size());
+        REQUIRE(tmp == "orld");
+    }
+    
+    SECTION("rotated file size test") {
+        TestApp app;
+        auto scoped_dir = ScopedTemporaryDirectoryProvider(L"logging-test");
+        
+        auto const test_file_name = L"logging-test.log";
+        auto const test_file_path = wxFileName(scoped_dir.GetPath(), test_file_name);
+        
+        auto st = std::make_shared<FileLoggingStrategy>(test_file_path.GetFullPath().ToStdWstring());
+        
+        st->EnableRedirectionToDebugConsole(false);
+        st->SetFileSizeLimit(10000);
+        
+        for(int i = 0; i < 1000; ++i) {
+            st->OutputLog(L"hello world. hello world. hello world.");
+            auto const file_size = get_file_size(test_file_path);
+            if(st->GetFileSizeLimit() < file_size) { // reduce too-much tracing
+                REQUIRE(st->GetFileSizeLimit() >= file_size);
+            }
+        }
+        
+        st->SetFileSizeLimit(1000);
+        st->OpenPermanently();
+        REQUIRE(st->GetFileSizeLimit() >= get_file_size(test_file_path));
+    }
 }
